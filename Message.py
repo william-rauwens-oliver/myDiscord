e79bcd3d806fb0c6eb9519c1d7ea3e27bdf0be9b
jean <jean@gmail.com>
2024-02-08 14:18:21 +0100

    création de la classe message + ajout des méthodes init, add_reaction

---
+++ Message.py
@@ -0,0 +1,13 @@
+class Message:
+    def __init__(self, content, author, timestamp, reactions=None):
+        self.content = content
+        self.author = author
+        self.timestamp = timestamp
+        self.reactions = reactions if reactions is not None else []
+
+    def add_reaction(self, emoji, reactor):
+        self.reactions.append((emoji, reactor))
+
+    def text():
+        pass
+
