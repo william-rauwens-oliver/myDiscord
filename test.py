f51b367eccd50a8773d7a61c7cefede5cd7e0605
kenny-ali-source <kenny.ali@laplateforme.io>
2024-02-27 14:12:34 +0100

    ligne 7 ConnectionRefusedError

--- test.py
+++ test.py
@@ -1,59 +1,89 @@
 import threading
-import socket
+from socket import AF_INET, socket, SOCK_STREAM
+import time
+import mysql.connector
 
-# Connection Data
-host = '127.0.0.1'
-port = 33002
+class Server:
+    def __init__(self, host, port):
+        self.HOST = host
+        self.PORT = port
+        self.BUFSIZ = 1024
+        self.ADDR = (self.HOST, self.PORT)
+        self.SERVER = socket(AF_INET, SOCK_STREAM)
+        self.SERVER.bind(self.ADDR)
+        self.clients = {}
+        self.addresses = {}
+        self.db_config = {
+            'host': 'localhost',
+            'user': 'root',
+            'password': 'root',
+            'database': 'myDiscord'
+        }
+        self.mydb = mysql.connector.connect(**self.db_config)
 
-# Starting Server
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.bind((host, port))
-server.listen()
+    def insert_message_into_db(self, author, timestamp, content):
+        cursor = self.mydb.cursor()
+        sql = "INSERT INTO messages (author, content, timestamp) VALUES (%s, %s, %s)"
+        cursor.execute(sql, (author, content, timestamp))
+        self.mydb.commit()
+        cursor.close()
 
-# Lists For Clients and Their Nicknames
-clients = []
-nicknames = []
+    def run(self):
+        self.SERVER.listen()
+        print("Server is running on port", self.PORT)
+        thread_reception = threading.Thread(target=self.recevoir)
+        thread_reception.start()
+        thread_reception.join()  
+        if __name__ == "__main__":
+            HOST = '127.0.0.1'
+            PORT = 33002
+            nickname = input("Choose your nickname: ")
+            server = Server(HOST, PORT)
+            server.run(nickname)  
 
-# Sending Messages To All Connected Clients
-def broadcast(message):
-    for client in clients:
-        client.send(message)
+    def recevoir(self):
+        while True:
+            client, adresse = self.SERVER.accept()
+            print("Connecté avec {}".format(str(adresse)))
+            client.send('NICK'.encode('ascii'))
+            surnom = client.recv(1024).decode('ascii')
+            self.addresses[client] = adresse
+            self.clients[client] = surnom
+            print("Le surnom est {}".format(surnom))
+            self.diffusion("{} a rejoint!".format(surnom).encode('ascii'))
+            client.send('Connecté au serveur!'.encode('ascii'))
+            thread = threading.Thread(target=self.gerer, args=(client,))
+            thread.start()
 
-# Handling Messages From Clients
-def handle(client):
-    while True:
-        try:
-            # Broadcasting Messages
-            message = client.recv(1024)
-            broadcast(message)
-        except:
-            # Removing And Closing Clients
-            index = clients.index(client)
-            clients.remove(client)
-            client.close()
-            nickname = nicknames[index]
-            broadcast('{} left!'.format(nickname).encode('UTF-8'))
-            nicknames.remove(nickname)
-            break
-# Receiving / Listening Function
-def receive():
-    while True:
-        # Accept Connection
-        client, address = server.accept()
-        print("Connected with {}".format(str(address)))
+    def gerer(self, client):
+        while True:
+            try:
+                message = client.recv(1024)
+                if message:
+                    destinataires, contenu_message = message.decode('UTF-8').split('|')
+                    destinataires = destinataires.split(';')
+                    self.envoyer_a_destinataires(contenu_message.encode('UTF-8'), client, destinataires)
+            except:
+                surnom = self.clients[client]
+                self.diffusion('{} left!'.format(surnom).encode('UTF-8'))
+                del self.clients[client]
+                client.close()
+                break
 
-        # Request And Store Nickname
-        client.send('NICK'.encode('ascii'))
-        nickname = client.recv(1024).decode('ascii')
-        nicknames.append(nickname)
-        clients.append(client)
+    def diffusion(self, message):
+        for client in self.clients:
+            client.send(message)
 
-        # Print And Broadcast Nickname
-        print("Nickname is {}".format(nickname))
-        broadcast("{} joined!".format(nickname).encode('ascii'))
-        client.send('Connected to server!'.encode('ascii'))
+    def envoyer_a_destinataires(self, message, client_expediteur, destinataires):
+        for client, surnom in zip(self.clients, self.clients.values()):
+            if surnom in destinataires and client != client_expediteur:
+                try:
+                    client.send(message)
+                except:
+                    index = list(self.clients.keys()).index(client)
+                    del self.clients[client]
+                    client.close()
+                    surnom_supprime = list(self.clients.values())[index]
+                    self.diffusion('{} left!'.format(surnom_supprime).encode('UTF-8'))
 
-        # Start Handling Thread For Client
-        thread = threading.Thread(target=handle, args=(client,))
-        thread.start()
-receive()
+
--- test2.py
+++ test2.py
@@ -1,37 +1,52 @@
-import socket
 import threading
+import socket
 
-# Choosing Nickname
 nickname = input("Choose your nickname: ")
 
-# Connecting To Server
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect(('127.0.0.1', 33002))
 
-# Listening to Server and Sending Nickname
-def receive():
+def receive(nickname):  
     while True:
         try:
-            # Receive Message From Server
-            # If 'NICK' Send Nickname
             message = client.recv(1024).decode('UTF-8')
             if message == 'NICK':
                 client.send(nickname.encode('UTF-8'))
             else:
                 print(message)
         except:
-            # Close Connection When Error
-            print("An error occured!")
+            print("An error occurred!")
             client.close()
             break
-# Sending Messages To Server
+
+
+def run(self, nickname):  
+    print("Server is running on port", self.PORT)
+    thread_reception = threading.Thread(target=self.recevoir, args=(nickname,))  # Passer le pseudonyme
+    thread_reception.start()
+    thread_reception.join()
+
+
 def write():
     while True:
-        message = '{}: {}'.format(nickname, input(''))
-        client.send(message.encode('UTF-8'))
-# Starting Threads For Listening And Writing
-receive_thread = threading.Thread(target=receive)
-receive_thread.start()
+        try:
+            recipients = input("Enter recipient(s) separated by comma (or leave blank for all): ").split(',')
+            message_content = input("Enter message: ")
 
+            message = f"{';'.join(recipients)}|{nickname}: {message_content}"
+            client.send(message.encode('UTF-8'))
+        except OSError as e:
+            print("An error occurred:", e)
+            break
+
+receive_thread = threading.Thread(target=receive)
 write_thread = threading.Thread(target=write)
+
+receive_thread.start()
 write_thread.start()
+
+receive_thread.join()
+write_thread.join()
+
+
+print("Program ended.")
